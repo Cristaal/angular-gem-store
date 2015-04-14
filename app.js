43889f362953b484d76e0cb78bffa38a99ac4ced
Crystal Contreras <crystalcontrerasgrossman@gmail.com>
2015-04-13 21:32:28 -0700

    add work-in-progress for displaying multiple items on the page

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
   var app = angular.module('store', [ ]);
   
   app.controller('StoreController', function() {
-  	this.product = gem;
+  	this.product = gems;
   });	
 
-  var gem = {
+  var gems = [
+  {
   	name: 'Dodecahedron', 
   	price: 2.95,
-  	description: 'Some gems have hidden qualities beyond their luster, beyond their shie...Dodeca is one of those gems.',
+  	description: 'Some gems have hidden qualities beyond their luster, beyond their shine...Dodeca is one of those gems.',
+  	canPurchase: true,
+  	soldOut: false,
+  },
+  {
+  	name: 'Pentagonal Gem', 
+  	price: 5.95,
+  	description: 'If one could capture the rays of the sun, their eartly form would be that of a Pentagonal Gem.',
+  	canPurchase: true,
+  	soldOut: false,
   }
-})();
+]});
