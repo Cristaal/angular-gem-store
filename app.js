b383e25e18a3f0c92ff9273cdf0858f4373d2822
Crystal Contreras <crystalcontrerasgrossman@gmail.com>
2015-04-16 21:07:53 -0700

    add reviewCtrl

--- app.js
+++ app.js
@@ -5,13 +5,21 @@
   	this.products = gems;
   });
 
+  app.controlller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+    };
+  });
+
   app.controller('PanelController', function() {
     this.tab = 1;
 
     this.selectTab = function(setTab){
       this.tab = setTab;
     };
-    
+
     this.isSelected = function(checkTab){
       return this.tab === checkTab;
     };
